a080f1bc0fce5f1af076c6c96a17c7de9712cbbf
omar <omar.barrera02@uptc.edu.co>
2024-03-17 23:20:53 -0500

    Micro8

--- src/main/java/com/send/email/EmailApplication.java
+++ src/main/java/com/send/email/EmailApplication.java
@@ -2,8 +2,9 @@ package com.send.email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EmailApplication {
 
