c8486481972c3cd7f5bf39628acfb9261547f846
Omarhu52 <118152326+Omarhu52@users.noreply.github.com>
2024-03-17 22:34:25 -0500

    Update EmailApplication.java

--- src/main/java/com/send/email/EmailApplication.java
+++ src/main/java/com/send/email/EmailApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class EmailApplication {
 
 	public static void main(String[] args) {
